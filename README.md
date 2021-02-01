e18fc5304ec39122d19881e6749c1a9bd82a67ef
JaredWeav <jared.weaver2@gmail.com>
2021-02-01 16:04:44 -0500

    js updated, functioning as planned

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 
-# k
+# ReadMe Generator
 
-k
+Generates a quality README.md following a series of prompts in the terminal.
+
+[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
 
 ## Table of Contents
 
@@ -16,15 +18,29 @@ k
 
 To install the proper dependencies, please use the following command:
 
-k
+npm i
 
 ## Usage
 
+
+
 ## Contributions
+
+
 
 ## Tests
 
+npm i
+
 ## License
 
+This application is protected by the [Apache 2.0]](https://opensource.org/licenses/Apache-2.0). 
+
 ## Questions
+
+For any questions relating to this application, please reach out to me at the following:
+
+Email: jared.weaver2@gmail.com
+
+Github: [JaredWeaver](github.com/JaredWeaver)
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,11 +1,62 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  const mitBadge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+  const apacheBadge = '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+  const bsd3Badge = '[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)';
+  const gplBadge = '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+  const none = ' ';
+
+    if (license.includes('MIT')) {
+      return mitBadge;
+    }
+
+    if (license.includes('Apache 2.0')) {
+      return apacheBadge;
+    }
+
+    if (license.includes('BSD 3')) {
+      return bsd3Badge;
+    }
+
+    if (license.includes('GPL 3.0')) {
+      return gplBadge;
+    }
+
+    if (license.includes('none')) {
+      return none;
+    }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  const mitLink = '[MIT](https://opensource.org/licenses/MIT)';
+  const apacheLink = '[Apache 2.0]](https://opensource.org/licenses/Apache-2.0)';
+  const bsd3Link = '[BSD v3](https://opensource.org/licenses/BSD-3-Clause)';
+  const gplLink = '[GPL v3](https://www.gnu.org/licenses/gpl-3.0)';
+  const noneLink = ' ';
 
+  if (license.includes('MIT')) {
+    return mitLink;
+  }
+
+  if (license.includes('Apache 2.0')) {
+    return apacheLink;
+  }
+
+  if (license.includes('BSD 3')) {
+    return bsd3Link;
+  }
+
+  if (license.includes('GPL 3.0')) {
+    return gplLink;
+  }
+
+  if (license.includes('none')) {
+    return noneLink;
+  }
+}
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}
@@ -16,6 +67,8 @@ function generateMarkdown(data) {
 # ${data.title}
 
 ${data.description}
+
+${renderLicenseBadge(data.license)}
 
 ## Table of Contents
 
@@ -34,15 +87,31 @@ ${data.runTests}
 
 ## Usage
 
+${data.usage}
+
 ## Contributions
+
+${data.contributions}
 
 ## Tests
 
+${data.runTests}
+
 ## License
+
+This application is protected by the ${renderLicenseLink(data.license)}. 
 
 ## Questions
 
+For any questions relating to this application, please reach out to me at the following:
+
+Email: ${data.email}
+
+Github: [${data.username}](github.com/${data.username})
+
 `;
+
+
 }
 
 module.exports = generateMarkdown;
